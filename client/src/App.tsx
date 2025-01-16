1c30185d0f51f8ba352f94ba87d86ad2b55c3b7a
Olivier BYIRINGIRO <olivierbyiringiro025@gmail.com>
2025-01-16 07:48:18 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/Footer";
 
-function App() {
+function App() { 
   return (
     <div className="App">
       <ToastContainer />
