219d630f17a657226eba6d12ca28956144755a7d
Olivier BYIRINGIRO <olivierbyiringiro025@gmail.com>
2025-02-08 16:32:43 +0200

    fx(HeroSection): Moved the background image inside herosection

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -1,29 +1,26 @@
 import { Link } from "react-router-dom";
+import bgImage from "../assets/home-new-bg-free-img.jpg";
 
 const HeroSection = () => {
   return (
     <section
-      className="relative bg-cover bg-center h-screen flex items-center font-lato"
+      className="relative bg-cover bg-center min-h-[55vh] md:min-h-[calc(100vh-80px)] flex items-center font-lato before:absolute before:inset-0 before:bg-gradient-to-t before:from-[#2c5a73] before:via-[#1B5C7E]/60 before:to-[#2c5a73]/30 before:opacity-70"
       style={{
         backgroundAttachment: "fixed",
-        zIndex: 10,
+        backgroundImage: `url(${bgImage})`,
       }}
     >
-      <div
-        className="absolute inset-0 bg-[#1B5C7E] bg-opacity-30"
-        style={{ zIndex: 11 }}
-      ></div>
-      <div className="container mx-auto flex items-center relative z-20 pt-0">
-        <div className="max-w-lg text-white ml-10">
-          <h1 className="text-6xl font-bold leading-tight">
-            Raining Offers For 
-            <span className="block text-6xl mt-2">Hot Summer!</span>
+      <div className="container mx-auto flex items-center justify-center md:justify-start relative z-10 pt-0">
+        <div className="max-w-lg text-white text-center md:text-left mx-4 md:ml-10">
+          <h1 className="text-4xl md:text-6xl font-bold leading-tight">
+            Raining Offers For
+            <span className="block text-4xl md:text-6xl mt-2">Hot Summer!</span>
           </h1>
-          <p className="text-2xl my-4">25% Off On All Products</p>
+          <p className="text-lg md:text-2xl my-4">25% Off On All Products</p>
 
           <Link
             to="/shop"
-            className="inline-block mt-6 px-6 py-3 bg-white text-[#2a6e9d] font-semibold shadow-md transition-all duration-300 hover:bg-black hover:text-white"
+            className="inline-block mt-6 w-full md:w-auto px-6 py-3 bg-white text-black font-semibold shadow-md transition-all duration-300 hover:bg-black hover:text-white"
           >
             Shop Now
           </Link>
@@ -31,7 +28,6 @@ const HeroSection = () => {
       </div>
     </section>
   );
-  
 };
 
 export default HeroSection;
