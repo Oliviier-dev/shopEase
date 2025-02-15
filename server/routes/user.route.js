789ade2cb6aef72afe5d0a597074d029518f0bd7
Olivier BYIRINGIRO <olivierbyiringiro025@gmail.com>
2025-02-15 11:18:46 +0200

    Implement a route to verify a logged in user

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -6,5 +6,6 @@ const userRoute = express.Router();
 userRoute.post('/register', UserController.userRegister);
 userRoute.post('/login', UserController.userLogin);
 userRoute.post('/logout', UserController.userLogout);
+userRoute.get('/me', UserController.verifyUser);
 
 export default userRoute;
